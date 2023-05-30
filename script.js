94812b66d324f5ee93a83daef67d63a1f14f2a31
tomi-avila <avilatesting@gmail.com>
2023-05-30 16:36:33 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert() {alert('Hola! esta es un alerta desde JavaScript');}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function(){
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
